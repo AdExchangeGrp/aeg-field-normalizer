3292c602f2731a1d1361ebf0c820c0c021e82da9
Justin Durham <justin.durham@icloud.com>
2017-02-24 17:09:31 -0500

    Node 6

--- src/index.js
+++ src/index.js
@@ -1,24 +1,39 @@
-'use strict';
+export function normalizeNumber (num) {
 
-export function normalizeNumber(num) {
 	let conversion = Number(num);
+
 	if (isNaN(conversion) || !isFinite(conversion)) {
+
 		conversion = 0;
+
 	}
+
 	return conversion;
+
 }
 
-export function normalizeNumberZeroOrGreater(num) {
+export function normalizeNumberZeroOrGreater (num) {
+
 	let conversion = normalizeNumber(num);
+
 	if (conversion < 0) {
+
 		conversion = 0;
+
 	}
+
 	return conversion;
+
 }
 
-export function normalizeField(field) {
+export function normalizeField (field) {
+
 	if (!field) {
+
 		field = 'BLANK';
+
 	}
+
 	return field.replace(':', '-');
+
 }
